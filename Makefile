b930143e6fbc46fb1458ae94af773a3cfdece31d
Stephen Marshall <stephenjohnmarshall2000@gmail.com>
2021-10-05 16:35:58 -0600

    reverted to Karl's code for cleanliness moving forward

--- Makefile
+++ Makefile
@@ -4,12 +4,36 @@ MAINSRC=$(SRC)/main.cpp
 BIN=bin/game.o
 LINUXFLAGS = -I/usr/include/SDL2 
 OSXFLAGS = -I/usr/local/Cellar/
+WINDOWSFLAGS = TODO
 SDL2CFLAGS=-D_REENTRANT
-SDL2LIBS=-lSDL2
+SDL2LIBS=-lSDL2 -lSDL2_mixer
 INCLUDES=include/useful.h
 
+# BIN: $(MAINSRC)
+# autodetect:
+# 	ifeq ($(OS),Windows_NT)
+# 		SDL2CFLAGS += $(WINDOWSFLAGS)
+# 	else
+# 		UNAME_S := $(shell uname -s)
+# 		ifeq ($(UNAME_S),Linux)
+# 			SDL2CFLAGS += $(LINUXFLAGS)
+# 		endif
+# 		ifeq ($(UNAME_S),Darwin)
+# 			SDL2CFLAGS += $(OSXFLAGS)
+# 		endif
+# 		# UNAME_P := $(shell uname -p)
+# 		# ifeq ($(UNAME_P),x86_64)
+# 		#     CCFLAGS += -D AMD64
+# 		# endif
+# 		# ifneq ($(filter %86,$(UNAME_P)),)
+# 		#     CCFLAGS += -D IA32
+# 		# endif
+# 		# ifneq ($(filter arm%,$(UNAME_P)),)
+# 		#     CCFLAGS += -D ARM
+# 		# endif
+# 	endif
 
-linux: $(BIN)
+linux:
 	g++ $(MAINSRC) -o $(BIN) $(LINUXFLAGS) $(SDL2CFLAGS) $(SDL2LIBS)
 osx: 
 	g++ $(MAINSRC) -o $(BIN) $(OSXFLAGS) $(SDL2CFLAGS) $(SDL2LIBS)
--- src/main.cpp
+++ src/main.cpp
@@ -1,162 +1,92 @@
+// Example program:
+// Using SDL2 to create an application window
+
 #include <iostream>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_mixer.h>
 #include <vector>
+#include <map>
+#include "Exception.hpp"
+#include "MediaManager.hpp"
+#include "Game.hpp"
+#include "Particle.hpp"
 
 using namespace std;
 
-class Exception {
-    string description;
-    public:
-    Exception(string newDescription){
-        description = newDescription;
-    }
-    void fun(/*exception *this */) {
-    }
-    friend ostream & operator << (ostream &out, const Exception &e){
-        return out << "Error: " << e.description << endl;
-    }
+class MyParticle:public Particle {
+	Mix_Chunk *sample;
+	public:
+	MyParticle(SDL_Renderer *ren,Animation *a,Mix_Chunk *newSample,SDL_Rect *src,
+	  double x,double y,double vx,double vy,double ax,double ay):
+	  Particle(ren,a,src,x,y,vx,vy,ax,ay) {
+		  sample=newSample;
+	}
+	void collision() {
+		Mix_PlayChannel(-1,sample,0);
+	}
 };
 
-class Game {
-    protected:
-    SDL_Window *window;
-    SDL_Renderer *ren;
-    int ticks; //total ticks since start
-    
-    public:
-    Game (string title, int w=640, int h=480){
-        SDL_Init(SDL_INIT_VIDEO);
-        window = SDL_CreateWindow(
-            title.c_str(),              //window title
-            SDL_WINDOWPOS_UNDEFINED,    //inital window x pos
-            SDL_WINDOWPOS_UNDEFINED,    //intial window y pos
-            w,                          //width, in px
-            h,                          //height, in px
-            SDL_WINDOW_OPENGL           //flags: renderer, etc
-        );
-
-        if(window==NULL){throw Exception("Failed to create window");}
-        ren = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-        if(ren==NULL){throw Exception("Failed to create renderer");}
-        ticks=SDL_GetTicks();
-    }
-
-    void run() {
-        int newTicks;
-        while(1){
-            newTicks=SDL_GetTicks();    
-            SDL_Event e;
-            if(SDL_PollEvent(&e)) {
-                if(e.type == SDL_QUIT){
-                    break;
-                }
-            }
-            update(double(newTicks-ticks) / 1000.0);
-            ticks=newTicks;
-            // SDL_Delay(100);
-        }
-    }
-
-    virtual void update(double dt)=0; //a derived class *has* to override update
-    ~Game() {
-        SDL_DestroyWindow(window);
-        SDL_DestroyRenderer(ren);
-        SDL_Quit();
-    }
-
-};
-class Particle {
-  SDL_Renderer *ren;
-  SDL_Texture *bitmapTex;
-  SDL_Rect *src;
-  SDL_Rect dest;
-  double x,y,vx,vy,ax,ay;
-  public:
-  Particle(SDL_Renderer *newRen,SDL_Texture *newBitmapTex, 
-           SDL_Rect *newSrc,
-           double newx=0.0,double newy=0.0,
-           double newvx=0.0,double newvy=0.0,
-           double newax=0.0,double neway=0.0) {
-	  src=newSrc;
-	  ren=newRen;
-	  bitmapTex=newBitmapTex;
-	  dest.w=src->w;
-      dest.h=src->h;
-      dest.x=newx;
-      dest.y=newy;
-      x=newx;
-      y=newy;
-      vx=newvx; // px/s
-      vy=newvy; // px/s
-      ax=newax;
-      ay=neway;  // px/s/s
-  }
-    void update(double dt) {
-        vx+=ax*dt; vy+=ay*dt; 
-        x+=vx*dt;  y+=vy*dt;
-        dest.x=(int)x;
-        dest.y=(int)y;
-        SDL_RenderCopy(ren, bitmapTex, src, &dest);
-    }
-};
-class MyGame:public Game{
-    SDL_Texture *bmpTexture;
-    SDL_Rect src,dest;
+class MyGame:public Game{	  
+    SDL_Rect src;
     vector<Particle *> particles;
-
-    public:
-    MyGame(int w=640, int h=480):Game("my first game", w,h) {
-        SDL_Surface *ob;
-
-        ob=SDL_LoadBMP("media/arduino.bmp");
-        if(ob==NULL){ throw Exception("Failed to load bitmap");}
-    
-        src.w=ob->w;
-        src.h=ob->h;
-        dest.w=src.w;
-        dest.h=src.h;
-        src.x=0;
-        src.y=0;
-        dest.x=0;
-        dest.y=0;
-
-        bmpTexture = SDL_CreateTextureFromSurface(ren, ob);
-        if(bmpTexture==NULL){ throw Exception("Failed to create bitmap texture");}
-        SDL_FreeSurface(ob);
-        for (int i=0;i<100;i++) { 
-            int vx=rand()%100-50;
-            int vy=rand()%100-50;
-            particles.push_back(new Particle(ren,bmpTexture,&src,100,100,vx,vy,0,-10));
-        }
-    }
-    void update(double dt){
-        SDL_RenderClear(ren);
-        
-        // my game code here
-        // dest.x++; //TODO: make this respond to dt
-        // dest.y++;
-
-        SDL_RenderClear(ren);
-        SDL_RenderCopy(ren, bmpTexture, &src, &dest);
-        SDL_RenderPresent(ren);
-        //SDL_Delay(100); //let the game handle this instead
-    }
-    ~MyGame(){
-        SDL_DestroyTexture(bmpTexture);
-    }
+    Animation a,b;
+	Mix_Chunk *sound;
+    int jx,jy;
+	public:
+	MyGame(int w=640,int h=480):Game("Karl was here",w,h) {
+	  sound=media->readWav("media/crash.wav");
+      for (int i=0;i<16;i++) { 
+		 int vx=rand()%500 - 250;
+		 int vy=rand()%500 - 250;
+		 a.read(media,"media/anim1.txt");
+	//	 SDL_Texture *bitmapTex=media->read("media/obsticle.bmp");
+		 src.x=0; src.y=0;
+		 SDL_QueryTexture(a.getTexture(), NULL, NULL, &src.w, &src.h);
+         particles.push_back(new MyParticle(ren,&a,sound,&src,w/2,h/2,vx,vy,0,50));
+         particles[i]->setBound(0,0,w,h);
+       }
+       jx=w/2;
+       jy=w/2;
+       b.read(media,"media/background.txt");
+       src.x=0; src.y=0; src.w=640; src.h=480;
+	}
+	void handleKeyUp(SDL_Event keyEvent) {
+	}
+	void handleKeyDown(SDL_Event keyEvent) {
+		//cout << "KeyPress" << endl;
+		if (keyEvent.key.keysym.sym==SDLK_SPACE)
+		  particles[0]->incVelocity(0,-100);
+	}
+	void handleButtonDown(SDL_Event joyEvent) {
+		if (joyEvent.jbutton.button==0 && joyEvent.jbutton.which==0)
+		  particles[0]->incVelocity(0,-100);
+	}
+	void handleAxisMovement(SDL_Event joyEvent) {
+		if (joyEvent.jaxis.axis==0 && joyEvent.jaxis.which==0)
+		  jx=((joyEvent.jaxis.value)*100)/32768;
+		if (joyEvent.jaxis.axis==1 && joyEvent.jaxis.which==0)
+		  jy=((joyEvent.jaxis.value)*100)/32768;
+		particles[0]->setAcceleration(jx,jy);
+	}
+	void update(double dt) {
+      SDL_RenderClear(ren);
+      b.update(dt);
+      
+      SDL_RenderCopy(ren, b.getTexture(), &src, &src);
+      for (unsigned i=0;i<particles.size();i++) 
+        particles[i]->update(dt);
+      SDL_RenderPresent(ren);
+	}
+	~MyGame() {
+	}
 };
 
-int main(int argc, char* argv[]){
-
+int main(int argc, char* argv[]) {
     try {
-        MyGame g;
-        g.run();
-
-    } catch (Exception e){
-        cerr << e;
-    }
-
-    return 0;
-
+		MyGame g;
+		g.run();
+	} catch (Exception e) {
+		cerr << e ;
+	}
+	return 0;
 }
